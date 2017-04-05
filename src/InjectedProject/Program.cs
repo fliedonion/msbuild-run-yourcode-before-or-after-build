9dd9f22c989255e6c13ae41b22598dc355841c7d
Takahiro KITAHARA <fliedonion@users.noreply.github.com>
2017-04-06 02:24:11 +0900

    Add sample code

---
+++ src/InjectedProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code is not important in this sample.
+        }
+    }
+}
